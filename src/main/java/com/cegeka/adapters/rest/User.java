6c150a8d874c5235cb87f9f7edc873c37c894e6d
pieterjanssencegeka <pieter.janssen@cegeka.com>
2023-06-28 15:27:59 +0200

    remove unecessary setters

--- src/main/java/com/cegeka/adapters/rest/User.java
+++ src/main/java/com/cegeka/adapters/rest/User.java
@@ -13,16 +13,9 @@ public class User {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     @XmlElement(name = "age")
     public int getAge() {
         return age;
     }
 
-    public void setAge(int age) {
-        this.age = age;
-    }
 }

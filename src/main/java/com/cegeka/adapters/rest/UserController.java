46f1b0b8e63cd54e61d95d69fe5c8abc7e8394db
pieterjanssencegeka <pieter.janssen@cegeka.com>
2023-06-29 07:59:19 +0200

    add implementation for pebble

--- src/main/java/com/cegeka/adapters/rest/UserController.java
+++ src/main/java/com/cegeka/adapters/rest/UserController.java
@@ -1,5 +1,8 @@
 package com.cegeka.adapters.rest;
 
+import io.pebbletemplates.pebble.PebbleEngine;
+import io.pebbletemplates.pebble.template.PebbleTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -7,27 +10,45 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.xml.sax.SAXException;
 
+import javax.xml.XMLConstants;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+import java.io.File;
+import java.io.IOException;
 import java.io.StringWriter;
+import java.util.Map;
 
 @RestController
 public class UserController {
 
+    private final PebbleEngine pebbleEngine;
+
+    @Autowired
+    public UserController(PebbleEngine pebbleEngine) {
+        this.pebbleEngine = pebbleEngine;
+    }
+
+
     @PostMapping("/generate-xml")
     public ResponseEntity<String> generateXml(@RequestBody User user) {
-            // Create XML using the user data
-            System.out.println("test1");
-            String xml = createXml(user);
+        // Create XML using the user data
+        System.out.println("test1");
+        String xml = createXml(user);
 
-            // Set the Content-Type header to "application/xml"
-            HttpHeaders headers = new HttpHeaders();
-            headers.setContentType(MediaType.APPLICATION_XML);
+        // Set the Content-Type header to "application/xml"
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_XML);
 
-            // Return the XML as the response
-            return new ResponseEntity<>(xml, headers, HttpStatus.OK);
+        // Return the XML as the response
+        return new ResponseEntity<>(xml, headers, HttpStatus.OK);
     }
 
     private String createXml(User user) {
@@ -43,5 +64,36 @@ public class UserController {
             // Handle JAXB exception
             throw new RuntimeException("Error creating XML", e);
         }
+    }
+
+
+    @PostMapping("/generate-xml-with-pebble")
+    public String generateXmlWithPebble(@RequestBody User user) throws IOException, SAXException {
+        // Render the Pebble template with the user data
+        PebbleTemplate template = pebbleEngine.getTemplate("user.xml");
+        StringWriter writer = new StringWriter();
+        template.evaluate(writer, Map.of("user", user));
+
+
+        String xml = writer.toString();
+        validate(xml);
+
+
+
+        return xml;
+    }
+
+    private void validate(String xml) throws SAXException, IOException {
+        File xsdFile = new File("templates/user.xsd");
+        SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+        Schema schema = schemaFactory.newSchema(xsdFile);
+
+        // Create a validator from the schema
+        Validator validator = schema.newValidator();
+
+        Source xmlSource = new StreamSource(xml);
+
+        // Validate the XML against the schema
+        validator.validate(xmlSource);
     }
 }
